8b39fd98eb34d15cb6c33bc5a71315aee05d350c
Rizajin <jarkko.vaisanen@live.fi>
2024-05-15 13:51:05 +0300

    node related stuff, napi, the gyp file etc

--- src/framework/almost_electron.h
+++ src/framework/almost_electron.h
@@ -1,6 +1,6 @@
 #ifndef ALMOST_ELECTRON_H
 #define ALMOST_ELECTRON_H
 
-__declspec(dllexport) /* TODO: find out if import lib or extern c */ double Test(double a, double b);
+__declspec(dllexport) double Test(double a, double b);
 
 #endif
--- src/node_plugin.cpp
+++ src/node_plugin.cpp
@@ -1,1 +1,26 @@
-// TODO: node stuff ...
+#include <napi.h>
+#include "framework/almost_electron.h"
+
+Napi::Number TestWrapper(const Napi::CallbackInfo& info)
+{
+    Napi::Env env = info.Env();
+
+    if (info.Length() < 2 || !info[0].IsNumber() || !info[1].IsNumber())
+    {
+        Napi::TypeError::New(env, "Number expected").ThrowAsJavaScriptException();
+    }
+
+    double arg0 = info[0].As<Napi::Number>().DoubleValue();
+    double arg1 = info[1].As<Napi::Number>().DoubleValue();
+    Napi::Number returnValue = Napi::Number::New(env, Test(arg0, arg1));
+
+    return returnValue;
+}
+
+Napi::Object Init(Napi::Env env, Napi::Object exports)
+{
+    exports.Set(Napi::String::New(env, "test"), Napi::Function::New(env, TestWrapper));
+    return exports;
+}
+
+NODE_API_MODULE(addon, Init)
